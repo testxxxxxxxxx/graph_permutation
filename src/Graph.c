043c70ffaadb32bd4391704cbe11bfd48885cdad
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-11-07 23:53:40 +0100

    create definition for function allocateGraph and generateGraph

--- src/Graph.c
+++ src/Graph.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 #include "../include/Graph.h"
+
+void allocateGraph(struct Graph *g, int n)
+{
+
+}
+void generateGNP(struct Graph *g, int n, float probability)
+{
+
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "Graph.h"
+#include "../include/Graph.h"
 
 int main(int argc, char *argv[])
 {
