505091a7048219083a79b990544add9475a86869
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-11-13 21:30:14 +0100

    created definition of allocateGraph function and declared vertices in struct Graph and declared and created definition of freeGraph function

--- include/Graph.h
+++ include/Graph.h
@@ -1,13 +1,18 @@
 struct Graph
 {
-    int **matrix;
+    int **matrix; //adjacency matrix
+    int vertices; //number vertices in Graph
 
 };
 
 //Allocate memory for Graph
 //n - number of vertices
 void allocateGraph(struct Graph *g, int n);
+//free memory Graph
+void freeGraph(struct Graph *g);
 //Generate graph model G(n,r)
 //n - number of vertices
 //probability - edge probability
 void generateGNP(struct Graph *g, int n, float probability);
+//free memory Graph
+void freeGraph(struct Graph *g);
--- src/Graph.c
+++ src/Graph.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../include/Graph.h"
 
 void allocateGraph(struct Graph *g, int n)
 {
+    g->matrix = (int **)malloc(n * sizeof(int *));
+
+    for(int i = 0; i < n; i++)
+    {
+        g->matrix[i] = (int *)malloc(n * sizeof(int));
+
+    }
+
+    g->vertices = n;
+
+}
+void freeGraph(struct Graph *g)
+{
+    for(int i = 0; i < g->vertices; i++)
+    {
+        //free memory for each row
+
+        free(g->matrix[i]);
+
+    }
+
+    //free memory all graph
+
+    free(g->matrix);
 
 }
 void generateGNP(struct Graph *g, int n, float probability)
