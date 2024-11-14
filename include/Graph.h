9dc7c6646ae9e4c9c990c9c5345412b2fd8f4885
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-11-14 20:22:55 +0100

    corrected comments

--- include/Graph.h
+++ include/Graph.h
@@ -5,14 +5,16 @@ struct Graph
 
 };
 
-//Allocate memory for Graph
-//n - number of vertices
+/*Allocate memory for Graph
+* n - number of vertices
+*/
 void allocateGraph(struct Graph *g, int n);
 //free memory Graph
 void freeGraph(struct Graph *g);
-//Generate graph model G(n,r)
-//n - number of vertices
-//probability - edge probability
+/*Generate graph model G(n,r)
+* n - number of vertices
+* probability - edge probability
+*/
 void generateGNP(struct Graph *g, int n, float probability);
 //free memory Graph
 void freeGraph(struct Graph *g);
