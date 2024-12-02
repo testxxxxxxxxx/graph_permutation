599e0eddeb5465e1d8757742da6c94790092f20d
testxxxxxxxxx <pppppmarcin@gmail.com>
2024-12-02 23:12:59 +0100

    created test case for functions

--- src/main.c
+++ src/main.c
@@ -3,6 +3,19 @@
 
 int main(int argc, char *argv[])
 {
+    //Driver code:
+
+    struct Graph *g;
+
+    printf("test_graph");
+
+    allocateGraph(g, 7);
+
+    generateGNP(g, 7, 0.85, 10);
+
+    int permutation[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    printf('%d', graphFindPermutation(g, permutation, 10));
 
     return 0;
 }
